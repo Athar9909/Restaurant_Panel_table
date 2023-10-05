fe24e1c0a4dbc1acd29855b76bb056cacb76d25d
Athar <atharkatheri@gmail.com>
2023-10-05 18:57:29 +0530

    athar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
        <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/restaurant/login" element={<Login />} />
         <Route path="/restaurant/login/forgot-Password" element={<ForgotPass />} />
         <Route
